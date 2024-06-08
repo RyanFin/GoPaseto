9217c481b884f7479a9b6e0c91ecfdd2a0ca1fbc
RyanFin <ryanfinlayson125@hotmail.com>
2024-06-08 23:25:33 +0100

    create, login and delete api complete

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"RyanFin/GoPaseto/server"
+	"log"
+)
+
+const (
+	addr = "0.0.0.0:8080"
+)
+
+func main() {
+	_, err := server.NewServer(addr)
+	if err != nil {
+		log.Fatal("Failed to start server.")
+	}
+}
